d9007dfb8243e3ef95c8f3d89dd7dcad86be1482
VincentMadoreC <vincentmc95@gmail.com>
2022-04-26 10:13:34 -0400

    added the readme and command line options

--- LicencePlateRecognition/main.py
+++ LicencePlateRecognition/main.py
@@ -1,13 +1,20 @@
 import correction
 import ocr
 import cv2
-import argparse
+import sys
 
 
-def main():
-    # img_path = "./images/bc18351.jpg"
+def main(argv):
     img_path = "./images/cfva648.jpg"
+    DEBUG_MODE = False
 
+    for option in argv:
+        if option in ('-d', '--debug'):
+            print('Running in DEBUG mode...')
+            DEBUG_MODE = True
+    if len(argv) > 0:
+        img_path = argv[0]
+    
     # This will only work when correction the code to get the bounding boxes has been completed
     # corrected_img = correction.correct(img_path, True)
     # ocr.ocr(corrected_img, True)
@@ -17,12 +24,11 @@ def main():
     # Below is sample test code.
 
     # For now, correction() only works if the bounding boxes have been hardcoded
-    correction.correct("images/perspective.jpg", debug_mode=True)
+    correction.correct("images/perspective.jpg", debug_mode=DEBUG_MODE)
 
     # ocr works with any image
     img = cv2.imread(img_path)
-    ocr.ocr(img, debug_mode=True)
-
+    ocr.ocr(img, debug_mode=DEBUG_MODE)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# ProjectCSI4900
+# LicensePlateRecognition
+
+## Installation
+1. Clone the repository
+2. Install Python if you haven't done so already
+3. Install OpenCV for python
+```
+pip install opencv-python
+```
+
+## How to use
+Run the program using your IDE if you can. There are default values for the image used and the debug mode.
+Or use the command line:
+```
+ProjectCSI4900\LicencePlateRecognition> python main.py ./images/cvfa648.jpg --debug
+```
+
+## Important notes
+Not all characters have examples in the standard yet. When running the code with a plate containing numbers that are not in the dataset, make sure to save the characters (by enabling the identified code) and manually add them to the dataset.  Search for the comment “enable this line to create new images to use as standard”.
+
+The program is not really optimized yet. Expect it to take 20-30 seconds to run on slower hardware.
+
+Even though license seems to often be written with a 's', the government of Ontario writes it as licenCe https://www.ontario.ca/page/renew-your-licence-plate :)
